dcbed8ee8edb63b537f9e4853821379b3fc97904
Zeepeen <zeepeen.officiel@gmail.com>
2021-11-04 09:26:11 +0100

    Add Windows compatibility with Flask server

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 """This file serves as the entry point of the backend."""
 
 from sys import argv
-
+import platform
 
 def main() -> None:
     """The actual entry point."""
@@ -21,9 +21,13 @@ def main() -> None:
         app.run(port=9678, host="127.0.0.1")
         return
 
-    from bjoern import run
+    if platform.system() == "Windows":
+        from waitress import serve
+        serve(app,host="0.0.0.0", port=9678)
+    elif platform.system() == "Linux": # Maybe test with "Java" and MacOS ("Darwin") for the compatibilty of bjoern
+        from bjoern import run
 
-    run(app, "0.0.0.0", 9678)
+        run(app, "0.0.0.0", 9678)
 
 
 if __name__ == '__main__':
